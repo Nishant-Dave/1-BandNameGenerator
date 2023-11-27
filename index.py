aca3daa0a5fe43f54f5e03fac9a126243d8c3357
Dexter ND <dexternd9@gmail.com>
2023-11-27 21:42:17 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Generator..!")
+
+city = input("Enter the city you grew up in: ")
+pet = input("Enter the name of your pet: ")
+
+bandName = city + " " + pet
+
+#the input cursor shows on a new line:
+print("Your band name is: ", bandName)
